21a2b18f431894b2211e77d1ef8120f0b8d96f69
ijaihari <sjaiharinataraj@gmail.com>
2026-07-06 14:24:00 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import sitemapPlugin from 'vite-plugin-sitemap'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import sitemapPlugin from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), sitemapPlugin()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemapPlugin({
+      hostname: "https://jaiharinataraj.com",
+    }),
+  ],
+});
